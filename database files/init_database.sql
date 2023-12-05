b7faa7152abeeb1880eee4067744c77cc88fffad
Thiago Marsal Farias <thiagomarsal.farias@gmail.com>
2023-12-05 13:10:19 -0700

    Update Database init file

--- database files/init_database.sql
+++ database files/init_database.sql
@@ -20,11 +20,15 @@ CREATE TABLE Products (
     FOREIGN KEY (category_id) REFERENCES Categories(category_id)
 );
 
--- Insert some sample products
-INSERT INTO Products (product_name, product_description, price, category_id) VALUES
-    ('Laptop', 'High performance laptop', 1200.00, 1),
-    ('T-shirt', 'Cotton round neck t-shirt', 20.00, 2),
-    ('Introduction to PHP', 'Book about PHP programming', 30.00, 3);
+-- CREATE Customer table
+CREATE TABLE `Customers` (
+  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`)
+);
 
 -- Create Orders table
 CREATE TABLE Orders (
@@ -37,5 +41,20 @@ CREATE TABLE Orders (
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
--- Add foreign key index to optimize the database
-ALTER TABLE Orders ADD INDEX (product_id);
+-- Add indexes to optimize the database
+ALTER TABLE Orders ADD INDEX idx_product_id (product_id);
+ALTER TABLE Customers ADD INDEX idx_email (email);
+ALTER TABLE Customers ADD INDEX idx_phone (phone);
+
+-- Insert some sample products
+INSERT INTO Products (product_name, product_description, price, category_id) VALUES
+    ('Laptop', 'High performance laptop', 1200.00, 1),
+    ('T-shirt', 'Cotton round neck t-shirt', 20.00, 2),
+    ('Introduction to PHP', 'Book about PHP programming', 30.00, 3);
+
+-- Insert some sample customers
+INSERT INTO `Customers` (`first_name`, `last_name`, `email`, `phone`) VALUES
+('John', 'Doe', 'john.doe@example.com', '123-456-7890'),
+('Alice', 'Smith', 'alice.smith@example.com', '987-654-3210'),
+('Bob', 'Johnson', 'bob.johnson@example.com', NULL),
+('Eva', 'Williams', 'eva.williams@example.com', '555-123-4567');
